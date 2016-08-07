34a727a484ef540195d0be29f87e5e9837b88c5e
Ahmed Elkheshin <ahmed.elkheshin@gmail.com>
2016-08-07 19:17:38 -0400

    Full CRUD functionality

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -35,6 +35,13 @@ class DaysController < ApplicationController
     end
   end
 
+  def destroy
+    @day = Day.find(params[:id])
+    @day.destroy
+
+    redirect_to days_path
+  end
+
   private
   def day_params
     params.require(:day).permit(:date, :total_calories)
