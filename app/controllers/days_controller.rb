cd43e7c1db59922fbaf17d73bc2f87d0f2aa86b0
Ahmed Elkheshin <ahmed.elkheshin@gmail.com>
2016-08-07 17:45:20 -0400

    added create and show features

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,4 +1,20 @@
 class DaysController < ApplicationController
+  def show
+    @day = Day.find(params[:id])
+  end
+
   def new
   end
+
+  def create
+    @day = Day.new(day_params)
+
+    @day.save
+    redirect_to @day
+  end
 end
+
+  private
+  def day_params
+    params.require(:day).permit(:date, :total_calories)
+  end
---
+++ db/migrate/20160807212615_create_days.rb
@@ -0,0 +1,10 @@
+class CreateDays < ActiveRecord::Migration[5.0]
+  def change
+    create_table :days do |t|
+      t.string :date
+      t.integer :total_calories
+
+      t.timestamps
+    end
+  end
+end
